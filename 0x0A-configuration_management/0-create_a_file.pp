0f075fc17343cdb1732421f879a232e57559904c
Abadamsi <abbabadamasi33@gmail.com>
2023-11-27 14:30:13 -0800

    1st file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# This is the first Puppet script
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,4 @@
+0x0A-configuration_management
+
+This project is simply a set of tasks to familiarize you with the basic level syntax 
+which is virtually identical in newer versions of Puppet.
