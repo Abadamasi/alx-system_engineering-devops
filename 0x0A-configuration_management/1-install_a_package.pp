72498864cb346150437f92d3b4acfe3253b0fcf3
Abadamsi <abbabadamasi33@gmail.com>
2023-11-27 14:39:11 -0800

    Install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,6 +3,11 @@
 # Include the stdlib module
 include stdlib
 
+# Define a class for installing Flask
+class { 'python':
+  version => 'system',
+}
+
 # Install Flask using pip3
 package { 'flask':
   ensure   => '2.1.0',
