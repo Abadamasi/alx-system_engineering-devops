bd046c9b8ff7d8c02ad33630f5c06a5084f34b40
Abadamsi <abbabadamasi33@gmail.com>
2023-11-28 00:10:01 -0800

    killmenow

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 #this is puppet manifet that kills the process 
 
-exec { 'killmenow':
-  command     => 'pkill -f my_process_killmenow',
-  refreshonly => true,
+exec { 'pkill':
+  command     => 'pkill killmenow',
+  refreshonly => shell,
 }
 
