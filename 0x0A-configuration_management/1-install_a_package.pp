0ae315f9b6efc302502605c8ffc47ec741b5995a
Abadamasi <abbabadamasi33@gmail.com>
2024-02-28 15:01:02 -0800

    install_package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,16 @@
 # Install a specific version of Flask (2.1.0)
 
-package { 'install_flask':
-  ensure   => '2.1.0'
-  provider => 'pip3'
+exec { 'install_flask':
+    ensure     => '2.1.0',
+    provider   => 'pip3',
 
 }
+
+#Below is code for installing Werkzeug
+exec { 'install_werkzeug':
+    command     => '/usr/bin/pip3 install Werkzeug',
+    path        => '/usr/local/bin:/usr/bin:/bin',
+    unless      => '/usr/bin/pip3 show Werkzeug | grep -q "Name: Werkzeug"',
+    provider    => 'shell',
+    logoutput   => true,
+  }
